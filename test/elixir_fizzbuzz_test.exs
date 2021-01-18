4282fd249d57f8d5894aa82bbf5ceccac26756c6
kevinmartiniano <kevin.martiniano123@gmail.com>
2021-01-18 17:40:13 -0300

    Criando teste para caso de sucesso

--- test/elixir_fizzbuzz_test.exs
+++ test/elixir_fizzbuzz_test.exs
@@ -2,6 +2,13 @@ defmodule ElixirFizzbuzzTest do
   use ExUnit.Case
 
   describe "build/1" do
+    test "when a valid file is provided, return the converted list" do
+      expected_respose = {:ok, [1, 2, :fizz, 4, :buzz, :buzz, :fizzbuzz, :buzz]}
+
+      file_name = "numbers.txt"
+
+      assert ElixirFizzbuzz.build(file_name) == expected_respose
+    end
 
     test "when an invalid file is provided, return an error" do
       expected_respose = {:error, "Error reading the file: enoent"}

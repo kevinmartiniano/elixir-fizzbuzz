b76894a0a39191dad25dbc29e4e8e445b345b532
kevinmartiniano <kevin.martiniano123@gmail.com>
2021-01-18 17:22:04 -0300

    Transformando as funções privadas

--- lib/elixir_fizzbuzz.ex
+++ lib/elixir_fizzbuzz.ex
@@ -6,22 +6,23 @@ defmodule ElixirFizzbuzz do
     |> handle_file_head()
   end
 
-  def handle_file_head({:ok, result}) do
+  defp handle_file_head({:ok, result}) do
     result
     |> String.split(",")
     |> Enum.map(&convert_and_evaluate_numbers/1)
   end
 
-  def handle_file_head({:error, reason}), do: "Error reading the file: #{reason}"
+  defp handle_file_head({:error, reason}), do: "Error reading the file: #{reason}"
 
-  def convert_and_evaluate_numbers(number) do
+  defp convert_and_evaluate_numbers(number) do
     number
     |> String.to_integer()
     |> fizz_buzz()
   end
 
-  def fizz_buzz(number) when rem(number, 3) == 0 and rem(number, 5) == 0, do: :fizzbuzz
-  def fizz_buzz(number) when rem(number, 3) == 0, do: :fizz
-  def fizz_buzz(number) when rem(number, 5) == 0, do: :buzz
-  def fizz_buzz(number), do: number
+  defp fizz_buzz(number) when rem(number, 3) == 0 and rem(number, 5) == 0, do: :fizzbuzz
+  defp fizz_buzz(number) when rem(number, 3) == 0, do: :fizz
+  defp fizz_buzz(number) when rem(number, 5) == 0, do: :buzz
+  defp fizz_buzz(number), do: number
+
 end

47af960226667c13e7c47431f22056ebe9b3fcad
kevinmartiniano <kevin.martiniano123@gmail.com>
2021-01-18 15:43:38 -0300

    Criando função para ler arquivo de texto, separar conteúdo pelas virgulas e converter para integer.

--- lib/elixir_fizzbuzz.ex
+++ lib/elixir_fizzbuzz.ex
@@ -1,18 +1,16 @@
 defmodule ElixirFizzbuzz do
-  @moduledoc """
-  Documentation for `ElixirFizzbuzz`.
-  """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> ElixirFizzbuzz.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def build(file_name) do
+    file_name
+    |> File.read()
+    |> handle_file_head()
   end
+
+  def handle_file_head({:ok, result}) do
+    result
+    |> String.split(",")
+    |> Enum.map(&String.to_integer/1)
+  end
+
+  def handle_file_head({:error, reason}), do: reason
 end

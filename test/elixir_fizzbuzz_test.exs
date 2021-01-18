246fefa65451a0f2dc802a56d19e011b02f5d75e
kevinmartiniano <kevin.martiniano123@gmail.com>
2021-01-18 17:39:49 -0300

    Criando teste para caso de falha.

--- test/elixir_fizzbuzz_test.exs
+++ test/elixir_fizzbuzz_test.exs
@@ -1,8 +1,14 @@
 defmodule ElixirFizzbuzzTest do
   use ExUnit.Case
-  doctest ElixirFizzbuzz
 
-  test "greets the world" do
-    assert ElixirFizzbuzz.hello() == :world
+  describe "build/1" do
+
+    test "when an invalid file is provided, return an error" do
+      expected_respose = {:error, "Error reading the file: enoent"}
+
+      file_name = "random.txt"
+
+      assert ElixirFizzbuzz.build(file_name) == expected_respose
+    end
   end
 end
